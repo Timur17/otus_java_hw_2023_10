f3da400ca9ffeedf98893627615d0d170d0c2161
TimurTS <for_work17@mail.ru>
2023-12-26 13:44:11 +0300

    hw solid

---
+++ hw12-solid/build.gradle.kts
@@ -0,0 +1,12 @@
+dependencies {
+    implementation ("ch.qos.logback:logback-classic")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
