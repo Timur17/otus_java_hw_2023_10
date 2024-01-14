e3d8f20e4ec256cc356f6fcfce253537ec9915cf
TimurTS <for_work17@mail.ru>
2024-01-14 19:29:23 +0300

    add @Setter(AccessLevel.NONE) to field amount in Cell.class

--- hw12-solid/src/main/java/ru/otus/model/Cell.java
+++ hw12-solid/src/main/java/ru/otus/model/Cell.java
@@ -1,10 +1,13 @@
 package ru.otus.model;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 public class Cell {
     private final int denomination;
+    @Setter(AccessLevel.NONE)
     private int amount;
 
     public int showBalance() {
