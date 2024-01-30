d045344837c7793096292476a217c5570ba81230
TimurTS <for_work17@mail.ru>
2024-01-30 21:41:45 +0300

    correct 30/01/24

--- hw15-patterns/src/main/java/ru/otus/processor/homework/DateTimeProviderImpl.java
+++ hw15-patterns/src/main/java/ru/otus/processor/homework/DateTimeProviderImpl.java
@@ -3,14 +3,8 @@ package ru.otus.processor.homework;
 import java.time.LocalDateTime;
 
 public class DateTimeProviderImpl implements DateTimeProvider {
-    private LocalDateTime localDateTime;
 
-    @Override
     public LocalDateTime getDate() {
-        if (localDateTime == null) {
-            localDateTime = LocalDateTime.now();
-            return LocalDateTime.now();
-        }
-        return localDateTime;
+        return LocalDateTime.now();
     }
 }
--- hw15-patterns/src/test/java/ru/otus/processor/ProcessorEvenErrorTest.java
+++ hw15-patterns/src/test/java/ru/otus/processor/ProcessorEvenErrorTest.java
@@ -2,21 +2,24 @@ package ru.otus.processor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import ru.otus.handler.ComplexProcessor;
 import ru.otus.model.Message;
-import ru.otus.processor.homework.DateTimeProviderImpl;
+import ru.otus.processor.homework.DateTimeProvider;
 
 class ProcessorEvenErrorTest {
 
     @Test
-    @DisplayName("Тестируем ProcessorThrowException")
-    void handleProcessorThrowExceptionIfNonEvesSecondTest() {
-        var message = new Message.Builder(1L).field11("field7").build();
+    @DisplayName("Тестируем ProcessorThrowException нечетная секунда")
+    void handleProcessorThrowExceptionIfEvesSecondTest() {
+        DateTimeProvider dateTimeProvider = Mockito.mock(DateTimeProvider.class);
+        Mockito.when(dateTimeProvider.getDate()).thenReturn(LocalDateTime.now().withSecond(1));
 
-        DateTimeProviderImpl dateTimeProvider = new DateTimeProviderImpl();
+        var message = new Message.Builder(1L).field11("field7").build();
 
         Processor processor1 = new ProcessorEvenError(dateTimeProvider);
 
@@ -29,10 +32,28 @@ class ProcessorEvenErrorTest {
         assertThat(result).isEqualTo(message);
 
         ProcessorEvenError proc = (ProcessorEvenError) processor1;
-        if ((dateTimeProvider.getDate().getSecond() % 2) == 0) {
-            assertThat(proc.getRuntimeException()).isNotNull();
-        } else {
-            assertThat(proc.getRuntimeException()).isNull();
-        }
+        assertThat(proc.getRuntimeException()).isNull();
+    }
+
+    @Test
+    @DisplayName("Тестируем ProcessorThrowException четная секунда")
+    void handleProcessorThrowExceptionIfNonEvesSecondTest() {
+        DateTimeProvider dateTimeProvider = Mockito.mock(DateTimeProvider.class);
+        Mockito.when(dateTimeProvider.getDate()).thenReturn(LocalDateTime.now().withSecond(2));
+
+        var message = new Message.Builder(1L).field11("field7").build();
+
+        Processor processor1 = new ProcessorEvenError(dateTimeProvider);
+
+        var processors = List.of(processor1);
+
+        var complexProcessor = new ComplexProcessor(processors, (ex) -> {});
+
+        var result = complexProcessor.handle(message);
+
+        assertThat(result).isEqualTo(message);
+
+        ProcessorEvenError proc = (ProcessorEvenError) processor1;
+        assertThat(proc.getRuntimeException()).isNotNull();
     }
 }
