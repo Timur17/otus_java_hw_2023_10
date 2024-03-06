935b93b3967804a3bf0d52f262a5496fa83a5ae6
Timur17 <TimurTs17@yandex.ri>
2024-03-06 17:29:28 +0300

    save code

---
+++ hw28-springDataJdbc/src/main/java/ru/otus/services/ClientServiceImpl.java
@@ -0,0 +1,50 @@
+package ru.otus.services;
+
+import org.springframework.stereotype.Service;
+import ru.otus.model.Client;
+import ru.otus.repository.ClientRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    private final ClientRepository clientRepository;
+
+    private final Random random = new Random();
+
+    public ClientServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Optional<Client> findById(long id) {
+        return clientRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Client> findByName(String name) {
+        return clientRepository.findByName(name);
+    }
+
+    @Override
+    public Client findRandom() {
+        List<Client> clients = clientRepository.findAll();
+        return clients.stream()
+                .skip(random.nextInt(clients.size() - 1))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public Optional<Client> save(Client client) {
+        return Optional.ofNullable(clientRepository.save(client));
+    }
+}
